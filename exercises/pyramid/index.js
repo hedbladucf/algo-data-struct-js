3d24e088f6075822cb6204b0d4be167b50c8aa91
oscar <ohedblad@flhi.com>
2019-11-12 16:52:13 -0500

    another

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,23 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// SOLUTION #1
+function pyramid(n) {
+  const midPoint = Math.floor((2 * n - 1) / 2);
+
+  for(let row = 0; row < n; row++) {
+    let level = '';
+
+    for(let column = 0; column < 2 * n - 1; column++) {
+      if(midPoint - row <= column && midPoint + row >= column) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
